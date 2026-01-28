9744e89f152f0ea776197f1f9937410e461ae778
rodrigofprates2033-rgb <rodrigo.f.prates2033@gmail.com>
2026-01-28 07:05:17 +0000

    Remove padding-top da área principal da home
    Co-authored-by: Ona <no-reply@ona.com>

--- client/src/pages/ImpostorGame.tsx
+++ client/src/pages/ImpostorGame.tsx
@@ -1120,7 +1120,7 @@ const HomeScreen = () => {
       />
 
       {/* Main content area - flex-grow to push footer down */}
-      <div className="flex-1 flex flex-col items-center justify-center pt-20 md:pt-24 px-4 relative z-20">
+      <div className="flex-1 flex flex-col items-center justify-center pt-0 px-4 relative z-20">
         {/* Mobile action buttons - above the card */}
         <MobileActionButtons onDonateClick={() => setIsDonationOpen(true)} />
 
